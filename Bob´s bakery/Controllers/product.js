fa16424e57e2cf0c5432550bfcd03b671af638d3
jonasa64 <hvidovrefifa@gmail.com>
2020-01-29 10:53:38 +0100

    Updted products

---
+++ Bob´s bakery/Controllers/product.js
@@ -0,0 +1,20 @@
+const mapper = require('../mapper/productMapper');
+
+exports.getProduct = async (req,res) => {
+    const id = req.params.id;
+    try {
+        const product = await mapper.getProduct(id);
+        return res.render("products", {products: product});
+    }catch (e) {
+        return res.send('error');
+    }
+}
+
+exports.getProducts = async (req, res) => {
+    try {
+        const products = await mapper.getAllProducts();
+        return  res.render("products", {products:products });
+    }catch (error) {
+        return res.send("error");
+    }
+}
--- Bob´s bakery/routes/products.js
+++ Bob´s bakery/routes/products.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mapper = require('../mapper/productMapper');
+const productComntroller = require('../Controllers/product');
 const sessionChecker = require('../middeleware')
-router.get("/", sessionChecker, (req,res) => {
-    mapper.getAllProducts().then(p =>{
-        console.log(req.session)
-        res.render("products", {products:p });
-    } );
+
+router.get("/", sessionChecker, productComntroller.getProducts() );
    
-
-    //res.render("products");
-});
-
-
-router.get("/:id", sessionChecker, (req,res) => {
-    const id = req.params.id;
-    res.render("products", {products: mapper.getAllProducts(id)});
-});
+router.get("/:id", sessionChecker, productComntroller.getProduct());
 
 
 module.exports = router;
