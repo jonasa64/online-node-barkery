fe296de20c4125513a6c487f6a0478a979508fdd
jonasa64 <hvidovrefifa@gmail.com>
2020-01-29 10:36:09 +0100

    Update users.js

--- Bob´s bakery/routes/users.js
+++ Bob´s bakery/routes/users.js
@@ -1,34 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator')
-const mapper = require('../mapper/userMapper');
 const userController = require('../Controllers/user');
 const sessionChecker = require('../middeleware')
 
 
 router.get('/login', userController.getLoginView());
 
-router.get('/', sessionChecker, (req, res) => {
-    req.flash("notify", "you are now logged in")       
-  
-    res.render('profil', {username : req.session.name, id: req.session.userid});
-   
-})
+router.get('/', sessionChecker, userController.getProfile());
 
 router.post('/login', userController.login());
 
-
-
-router.get('/signup', (req, res) => {
-    res.render('signup');
-});
+router.get('/signup', userController.getSingupView());
 
 router.post('/signup', userController.signup());
 
-router.post('logout', (req, res) => {
-    req.session.destroy();
-    res.render('login')
-})
+router.post('logout', userController.logout());
 
 
 module.exports = router;
